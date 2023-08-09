23acf60e759ea9ef79da65e11056cb05dc331332
Andreas Aakvik Gogstad <agogstad@gmail.com>
2023-08-09 08:17:51 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WordsPerMinute
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            buttonReset = new Button();
+            labelResult = new Label();
+            labelTime = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(484, 136);
+            textBox1.TabIndex = 0;
+            textBox1.KeyDown += handleKeys;
+            textBox1.KeyUp += updateResultKeyPress;
+            // 
+            // buttonReset
+            // 
+            buttonReset.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonReset.Location = new Point(12, 154);
+            buttonReset.Name = "buttonReset";
+            buttonReset.Size = new Size(75, 23);
+            buttonReset.TabIndex = 1;
+            buttonReset.Text = "Reset (ESC)";
+            buttonReset.UseVisualStyleBackColor = true;
+            buttonReset.Click += clickReset;
+            // 
+            // labelResult
+            // 
+            labelResult.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            labelResult.AutoSize = true;
+            labelResult.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelResult.Location = new Point(93, 155);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(68, 21);
+            labelResult.TabIndex = 2;
+            labelResult.Text = "WPM: 0";
+            // 
+            // labelTime
+            // 
+            labelTime.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            labelTime.AutoSize = true;
+            labelTime.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelTime.Location = new Point(219, 155);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(65, 21);
+            labelTime.TabIndex = 3;
+            labelTime.Text = "Time: 0";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += updateResultTimer;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(332, 159);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(141, 19);
+            checkBox1.TabIndex = 4;
+            checkBox1.Text = "Password (black dots)";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(508, 189);
+            Controls.Add(checkBox1);
+            Controls.Add(labelTime);
+            Controls.Add(labelResult);
+            Controls.Add(buttonReset);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Words Per Minute";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button buttonReset;
+        private Label labelResult;
+        private Label labelTime;
+        private System.Windows.Forms.Timer timer1;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+using System.CodeDom;
+
+namespace WordsPerMinute
+{
+    public partial class Form1 : Form
+    {
+        private bool testStarted = false;
+        private DateTime timeStarted = DateTime.MinValue;
+        private DateTime timeEnded = DateTime.MinValue;
+        //private DateTime timeElapsed = DateTime.MinValue;
+        private TimeSpan timeInterval = TimeSpan.Zero;
+        char passwordDotSmall = '\u2022';// or "\u25cf"
+        char passwordDotLarge = '\u25cf';
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.PasswordChar = passwordDotLarge;
+            textBox1.UseSystemPasswordChar = false;
+        }
+
+        private void updateResultTimer(object sender, EventArgs e)
+        {
+            UpdateResult();
+            if (!checkBox1.Checked)
+            {
+                textBox1.UseSystemPasswordChar = true;
+
+            }
+            else
+            {
+                textBox1.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void updateResultKeyPress(object sender, KeyEventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            if (testStarted)
+            {
+                float wpm = 0;
+                float cpm = 0;
+                if (timeStarted != DateTime.MinValue)
+                {
+                    timeInterval = timeEnded - timeStarted;
+                    cpm = 60 * (textBox1.Text.Length / (float)timeInterval.TotalSeconds);
+                    wpm = cpm / 5;
+                }
+                labelResult.Text = "WPM: " + ((int)wpm).ToString().PadLeft(3);
+
+                labelTime.Text = "Time: " + TimeSpanHMS(timeInterval).ToString().Substring(3, 5);
+            }
+            else
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    StartTest();
+                }
+            }
+        }
+
+        public TimeSpan TimeSpanHMS(TimeSpan time)
+        {
+            return new TimeSpan(time.Hours, time.Minutes, time.Seconds);
+        }
+
+        private void StartTest()
+        {
+            testStarted = true;
+            timeStarted = DateTime.Now;
+        }
+
+        private void handleKeys(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                e.Handled = true;
+                ResetTest();
+            }
+            else
+            {
+                timeEnded = DateTime.Now;
+            }
+        }
+
+        private void ResetTest()
+        {
+            textBox1.Text = string.Empty;
+            labelResult.Text = "WPM: ...";
+            labelTime.Text = "Time: 00:00";
+            testStarted = false;
+        }
+
+        private void clickReset(object sender, EventArgs e)
+        {
+            ResetTest();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                textBox1.UseSystemPasswordChar = true;
+
+            }
+            else
+            {
+                textBox1.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
